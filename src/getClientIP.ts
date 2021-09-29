27c1429ea9f8144d5cadc60af915888dc394ec97
冯黎明 <fengliming@le.com>
2021-09-29 10:39:26 +0800

    rename

--- src/getClientIP.ts
+++ src/getClientIP.ts
@@ -3,6 +3,9 @@
 /**-
   获取客户端IP，需要在nginx配置里加上`proxy_set_header X-Real-Ip $remote_addr;`
   -p req 原生 request 对象
+  -eg
+    // ctx 为 koa context
+    getClientIP(ctx.req)
 */
 export default function getClientIP(req) {
   return req.headers['x-real-ip'] ||
