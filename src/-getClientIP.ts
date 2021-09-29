f98cf8dee0a4918f9b56a07e8ac3fd975efda2bf
冯黎明 <fengliming@le.com>
2021-09-29 10:15:57 +0800

    rm

--- src/-getClientIP.ts
+++ src/-getClientIP.ts
@@ -4,7 +4,7 @@
   获取客户端IP，需要在nginx配置里加上`proxy_set_header X-Real-Ip $remote_addr;`
   -p req 原生 request 对象
 */
-export default function getClientIp(req) {
+export default function getClientIP(req) {
   return req.headers['x-real-ip'] ||
     req.headers['x-forwarded-for'] ||
     req.connection.remoteAddress ||
