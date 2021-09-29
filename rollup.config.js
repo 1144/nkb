127bba1b0cce6a2b45a58501ed93afcf8a9f589e
冯黎明 <fengliming@le.com>
2021-09-29 19:51:27 +0800

    优化rollup输出

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 module.exports = {
+  external: ['crypto'],
   output: {
     dir: '2kb',
     format: 'cjs',
     preserveModules: true,
     preferConst: true,
     strict: false,
+    interop: false,
   },
   onwarn(payload) {
     if (payload.code !== 'PREFER_NAMED_EXPORTS') {
