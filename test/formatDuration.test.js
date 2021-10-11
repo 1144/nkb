4dc5c8e4be9549160ab6425797de29f1208703b4
冯黎明 <fengliming@le.com>
2021-10-11 14:10:31 +0800

    增加测试用例

--- test/formatDuration.test.js
+++ test/formatDuration.test.js
@@ -14,8 +14,38 @@ describe('formatDuration', () => {
   )
 
   test(
+    formatDuration(615, 'h小时i分钟s秒'),
+    '0小时10分钟15秒'
+  )
+
+  test(
+    formatDuration(15, 'h小时i分钟s秒'),
+    '0小时0分钟15秒'
+  )
+
+  test(
+    formatDuration(15, 'H小时I分钟S秒'),
+    '00小时00分钟15秒'
+  )
+
+  test(
     formatDuration(90015, 'd天h小时i分钟s秒'),
     '1天1小时0分钟15秒'
+  )
+
+  test(
+    formatDuration(0, 'd天h小时i分钟s秒'),
+    '0天0小时0分钟0秒'
+  )
+
+  test(
+    formatDuration(-1, 'd天h小时i分钟s秒'),
+    '0天0小时0分钟0秒'
+  )
+
+  test(
+    formatDuration(615.5, 'i分钟s秒'),
+    '10分钟15秒'
   )
 
 })
