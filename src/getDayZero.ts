a4c18221f0e7ea85de6db393eab053e36370b25f
冯黎明 <fengliming@le.com>
2021-12-19 22:18:54 +0800

    1.3.1 getDayZero 默认返回毫秒

--- src/getDayZero.ts
+++ src/getDayZero.ts
@@ -1,16 +1,16 @@
 /**-
-  获取某日0点0分0秒的时间戳，返回秒或毫秒
-  -p time 时间对象，或时间戳（单位为秒或毫秒，自动判断是秒还是毫秒）
-  -p unit 返回值的单位，默认秒；传`ms`则返回毫秒
+  获取某日0点0分0秒的时间戳，返回毫秒或秒
+  -p time 时间对象，或时间戳（单位为毫秒或秒，自动判断是毫秒还是秒）
+  -p unit 返回值的单位，默认豪秒；传`s`则返回秒
   -eg
-    getDayZero(new Date()) // 返回秒
-    getDayZero(new Date(), 'ms') // 返回豪秒
-    getDayZero(1538159538) // => 1538150400
-    getDayZero(1538159538188) // => 1538150400
-    getDayZero(1538159538, 'ms') // => 1538150400000
-    getDayZero(1538159538188, 'ms') // => 1538150400000
+    getDayZero(new Date()) // 返回豪秒
+    getDayZero(new Date(), 's') // 返回秒
+    getDayZero(1538159538) // => 1538150400000
+    getDayZero(1538159538188) // => 1538150400000
+    getDayZero(1538159538, 's') // => 1538150400
+    getDayZero(1538159538188, 's') // => 1538150400
 */
-export default function getDayZero(time: Date | number, unit?: 'ms') {
+export default function getDayZero(time: Date | number, unit?: 's') {
   const t = time instanceof Date ? time : new Date(time > 9e9 ? time : time * 1000)
 
   t.setHours(0)
@@ -18,5 +18,5 @@ export default function getDayZero(time: Date | number, unit?: 'ms') {
   t.setSeconds(0)
   t.setMilliseconds(0)
 
-  return t.getTime() / (unit === 'ms' ? 1 : 1000)
+  return t.getTime() / (unit === 's' ? 1000 : 1)
 }
--- test/getDayZero.test.js
+++ test/getDayZero.test.js
@@ -3,13 +3,8 @@ import getDayZero from '../1kb/getDayZero.js'
 
 describe('getDayZero', () => {
 
-  const s = getDayZero(new Date)
-  const ms = getDayZero(new Date, 'ms')
-
-  test(
-    s > 1e9 && s < 9e9,
-    true
-  )
+  const ms = getDayZero(new Date)
+  const sec = getDayZero(new Date, 's')
 
   test(
     ms > 9e9,
@@ -17,28 +12,33 @@ describe('getDayZero', () => {
   )
 
   test(
+    sec > 1e9 && sec < 9e9,
+    true
+  )
+
+  test(
     getDayZero(1538159538),
-    1538150400
+    1538150400000
   )
 
   test(
     getDayZero(1538159538188),
+    1538150400000
+  )
+
+  test(
+    getDayZero(1538159538, 's'),
     1538150400
   )
 
   test(
-    getDayZero(1538159538, 'ms'),
-    1538150400000
-  )
-
-  test(
-    getDayZero(1538159538188, 'ms'),
-    1538150400000
+    getDayZero(1538159538188, 's'),
+    1538150400
   )
 
   test(
     getDayZero(1538159538, 'xxx'),
-    1538150400
+    1538150400000
   )
 
 })
