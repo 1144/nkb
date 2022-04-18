14a49c55c84df5cabb3498105346dcd26eebd970
冯黎明 <fengliming@le.com>
2022-04-18 19:51:16 +0800

    1.4.3 createObject支持传入prototype

--- src/createObject.ts
+++ src/createObject.ts
@@ -3,9 +3,10 @@ import { JSON } from './_type'
 type ValueProducer = (key: any, keys: any[]) => any
 
 /**-
-  根据keys数组创建纯对象，以数组元素为key，值为valueProducer处理的结果
+  根据keys数组创建对象，以数组元素为key，值为valueProducer处理的结果
   -p keys 键数组
   -p valueProducer 值生产器，返回`undefined`则当前key会被丢弃；不传则创建对象的值都为`true`
+  -p prototype 对象的原型链
   -eg
     createObject(['KEY1', 'KEY2'], (key) => key.toLowerCase())
     // => { KEY1: 'key1', KEY2: 'key2' }
@@ -15,8 +16,8 @@ type ValueProducer = (key: any, keys: any[]) => any
     createObject([1, 2, 3, 4], (key) => key < 3 ? true : undefined)
     // => { 1: true, 2: true }
 */
-export default function createObject(keys: any[], valueProducer?: ValueProducer) : JSON {
-  const object = Object.create(null)
+export default function createObject(keys: any[], valueProducer?: ValueProducer, prototype?: Object | null) : JSON {
+  const object = prototype === undefined ? {} : Object.create(prototype)
 
   if (valueProducer) {
     keys.forEach(key => {
--- test/createObject.test.js
+++ test/createObject.test.js
@@ -5,17 +5,27 @@ describe('createObject', () => {
 
   test(
     createObject(['KEY1', 'KEY2'], (key) => key.toLowerCase()),
-    deletePrototype({ KEY1: 'key1', KEY2: 'key2' })
+    { KEY1: 'key1', KEY2: 'key2' }
   )
 
   test(
     createObject(['KEY1', 'KEY2']),
+    { KEY1: true, KEY2: true }
+  )
+
+  test(
+    createObject(['KEY1', 'KEY2'], (key) => key.toLowerCase(), null),
+    deletePrototype({ KEY1: 'key1', KEY2: 'key2' })
+  )
+
+  test(
+    createObject(['KEY1', 'KEY2'], null, null),
     deletePrototype({ KEY1: true, KEY2: true })
   )
 
   test(
     createObject([1, 2, 3, 4], (key) => key < 3 ? true : undefined),
-    deletePrototype({ 1: true, 2: true })
+    { 1: true, 2: true }
   )
 
 })
