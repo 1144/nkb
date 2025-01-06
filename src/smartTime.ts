4625bd40ebedbac9007e053fc1d66d7e54d2c8b1
冯黎明 <fengliming@le.com>
2025-01-06 19:05:56 +0800

    1.6.2 优化智能时间

--- src/smartTime.ts
+++ src/smartTime.ts
@@ -4,32 +4,34 @@ const notNum = /\D/g
   智能格式化时间，格式化为 08:30 这样的字符串，非数字字符会被忽略
   -p short 简化的时间字符串
   -eg
-    smartTime('0910') // => 09:10
-    smartTime('030') // => 00:30
-    smartTime('810') // => 08:10
-    smartTime('9') // => 09:00
+    smartTime('0910') // => 9:10
+    smartTime('030') // => 0:30
+    smartTime('810') // => 8:10
+    smartTime('9') // => 9:00
+    smartTime('0910', true) // => 09:10
+    smartTime('030', true) // => 00:30
 */
-export default function smartTime(short: string) {
+export default function smartTime(short: string, leadingZero = false) {
   const str = short.replace(notNum, '')
   const length = str.length
 
   // 3 位以上，例如 0910
   if (length > 3) {
-    return compose(str.slice(0, 2), str.slice(2, 4))
+    return compose(str.slice(0, 2), str.slice(2, 4), leadingZero)
   }
   // 3 位，例如 030，810
   if (length > 2) {
-    return compose(str[0], str.slice(1))
+    return compose(str[0], str.slice(1), leadingZero)
   }
   // 3 位以下，只有小时，例如 9，20
   if (length) {
-    return compose(str, '0')
+    return compose(str, '0', leadingZero)
   }
 
   return ''
 }
 
-function compose(h: string, i: string) {
+function compose(h: string, i: string, leadingZero: boolean) {
   let hour = parseInt(h, 10)
   let min = parseInt(i, 10)
 
@@ -43,5 +45,5 @@ function compose(h: string, i: string) {
   if (!hour && !min) {
     return ''
   }
-  return (hour > 9 ? hour : '0' + hour) + ':' + (min > 9 ? min : '0' + min)
+  return (hour > 9 || !leadingZero ? hour : '0' + hour) + ':' + (min > 9 ? min : '0' + min)
 }
--- test/smartTime.test.js
+++ test/smartTime.test.js
@@ -5,26 +5,61 @@ describe('smartTime', () => {
 
   test(
     smartTime('0910'),
-    '09:10'
+    '9:10'
+  )
+
+  test(
+    smartTime('1230'),
+    '12:30'
   )
 
   test(
     smartTime('030'),
-    '00:30'
+    '0:30'
   )
 
   test(
     smartTime('810'),
-    '08:10'
+    '8:10'
   )
 
   test(
     smartTime('9'),
-    '09:00'
+    '9:00'
   )
 
   test(
     smartTime('x9'),
+    '9:00'
+  )
+
+  test(
+    smartTime('0910', true),
+    '09:10'
+  )
+
+  test(
+    smartTime('1230', true),
+    '12:30'
+  )
+
+  test(
+    smartTime('030', true),
+    '00:30'
+  )
+
+  test(
+    smartTime('810', true),
+    '08:10'
+  )
+
+  test(
+    smartTime('9', true),
+    '09:00'
+  )
+
+  test(
+    smartTime('x9', true),
     '09:00'
   )
 
